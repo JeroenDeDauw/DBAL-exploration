0b58d81c242bec18739aa7ea5bb26a0c208d6c1a
jeroendedauw <jeroendedauw@gmail.com>
2014-04-26 23:22:29 +0200

    Add some basic tests

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -1,9 +1,35 @@
 <?php
 
+use Doctrine\DBAL\DriverManager;
+
 class ConnectionTest extends PHPUnit_Framework_TestCase {
 
+	/**
+	 * @var \Doctrine\DBAL\Connection
+	 */
+	private $connection;
+
+	public function setUp() {
+		$config = new \Doctrine\DBAL\Configuration();
+
+		$connectionParams = array(
+			'driver' => 'pdo_sqlite',
+			'memory' => true,
+		);
+
+		$this->connection = DriverManager::getConnection( $connectionParams, $config );
+	}
+
 	public function testConnection() {
-		$this->assertTrue( true );
+		$this->assertTrue( $this->connection->connect() );
+		$this->assertFalse( $this->connection->connect() );
+	}
+
+	public function testListTables() {
+		$this->assertEquals(
+			array(),
+			$this->connection->getSchemaManager()->listTables()
+		);
 	}
 
 }
